26719dc809a285c048c06ade260c638cc65dd628
Cheuk Yin Ng <chucksys88@gmail.com>
2018-04-28 23:15:24 -0700

    Adds fb integration completely

--- app/static/base.js
+++ app/static/base.js
@@ -1,4 +1,37 @@
-function centralize(tag) {
-    let div = document.getElementsByClassName(tag)[0];
-    div.style.marginTop = '50%';
+// Initializae firebase
+var config = {
+    apiKey: 'AIzaSyB4bSmMVycec_35pi98syy9vQkjvePUo8M',
+    authDomain: 'stymer-ea133.firebase.com',
+    databaseURL: 'https://stymer-ea133.firebaseio.com/',
+    storageBucket: 'stymer-ea133.appspot.com'
+};
+firebase.initializeApp(config);
+
+var fDB = firebase.database();
+
+function secsToDate(s) {
+    let d = new Date(null);
+    d.setSeconds(s);
+    return d.toISOString().substr(11, 8);
 }
+
+function updateTimerValues(userID) {
+    // Link with firebase
+    let user = fDB.database().ref('users/' + userID);
+    user.on('value', function(snap) {
+        $('.time').text(secsToDate(snap));
+    });
+}
+
+function fbStatusChange(resp) {
+    if (resp === 'connected') {
+        let userID = resp['authResponse']['userID'];
+        updateTimerValues(userID);
+    }
+}
+
+$(function() {
+    //FB.getLoginStatus(function(resp) {
+    //    fbStatusChange(resp);
+    //});
+});
